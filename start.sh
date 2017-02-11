d2f97081ed429323e39b2b08533c2ade71479ca3
Marc Wäckerlin <marc@waeckerlin.org>
2017-02-11 13:49:21 +0100

    bugfixes - fix logging

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # setup logging
 ! test -e /var/log/mail.log || rm /var/log/mail.log
-ln -sf /proc/$$/fd/1 /var/log/mail.log
+ln -sf /proc/self/fd/1 /var/log/mail.log
 
 service postgrey status && service postgrey restart || service postgrey start
 sleep infinity
